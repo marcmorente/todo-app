cf124f35bfccbb75d24298acf56aff4ca0012146
Marc Morente <mmorente@inthinking.co.uk>
2023-06-08 10:00:46 +0200

    First commit

---
+++ src/Entity/TodoApp.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoAppRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TodoAppRepository::class)]
+class TodoApp
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $task = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTask(): ?string
+    {
+        return $this->task;
+    }
+
+    public function setTask(string $task): static
+    {
+        $this->task = $task;
+
+        return $this;
+    }
+}
